f4266d4f11f8d7cf8fffd9bf15b1f45c360f56aa
Poksi <Poksi@DESKTOP-5EO14RQ>
2022-11-19 16:51:18 +0100

    Add sql file

---
+++ App/Database/dbo.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName]   NVARCHAR (MAX) NULL,
+    [LastName]    NVARCHAR (MAX) NULL,
+    [Email]       NVARCHAR (MAX) NULL,
+    [Pesel]       NVARCHAR (11)  NULL,
+    [PhoneNumber] NVARCHAR (9)   NULL,
+    [Adress]      NVARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
